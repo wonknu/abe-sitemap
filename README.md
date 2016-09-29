56d81023ce6e3dd3951f3aec5fac769542e0fa5c
nicolaslabbe <nicolas.labbe@adfab.fr>
2016-09-29 14:53:04 +0200

    update with more conf

--- README.md
+++ README.md
@@ -9,15 +9,26 @@ abe add https://github.com/AdFabConnect/abe-sitemap.git
 # config
 
 ```json
-  "sitemap": {
+	"sitemap": {
   	"domain": "http://localhost:8000/",
   	"sitemapName": "index_sitemap.xml",
-  	"prorities": {
+  	"priorities": {
   		"article": "0.5"
   	},
   	"folders": {
-  		"regex": "^\/([a-zA-z-]*?\/",
+  		"regex": "^\/([a-zA-z-]*?)\/",
   		"sitemapName": "directory-sitemap-$1.xml"
   	}
   }
 ```
+
+Mandatory config:
+
+- domain
+
+Optional config:
+- sitemapName (change default sitemap.xml name)
+- priorities (key/value = template name/priority number)
+- folders (to split sitemap into multiple files)
+	- regex (the regex to extract folder)
+	- sitemapName (name with "$1" to replace with folder name for example)
--- process/sitemap.js
+++ process/sitemap.js
@@ -9,6 +9,42 @@ Array.prototype.forEach.call(process.argv, function(item) {
   }
 })
 
+
+function createXml(list, domain, priorities) {
+	var shouldSaveXml = false;
+	var xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+	xml += "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n";
+
+	Array.prototype.forEach.call(list, function(file) {
+		if (file.publish) {
+			shouldSaveXml = true;
+			var d = new Date(file.publish.date);
+			var month = d.getUTCMonth() + 1; //months from 1-12
+			var day = d.getUTCDate();
+			var year = d.getUTCFullYear();
+
+			var newdate = year + "-" + ((month < 10) ? "0" + month : month) + "-" + ((day < 10) ? "0" + day : day);
+
+			var priority = "0.5";
+			if(typeof priorities[file.abe_meta.template] !== 'undefined' && priorities[file.abe_meta.template] !== null) {
+				priority = priorities[file.abe_meta.template];
+			}
+
+		  xml += "  <url>\n";
+		  xml += "    <loc>" + domain + file.publish.html.replace(/^\//, '') + "</loc>\n";
+		  xml += "    <lastmod>" + newdate + "</lastmod>\n";
+		  xml += "    <priority>" + priority + "</priority>\n";
+		  xml += "  </url>\n";
+		}
+	})
+	xml += "</urlset>";
+
+	if (!shouldSaveXml) {
+		return false
+	}
+	return xml
+}
+
 if(typeof pConfig.ABEJS_PATH === 'undefined' || pConfig.ABEJS_PATH === null) {
   pConfig.ABEJS_PATH = ''
 }
@@ -23,53 +59,65 @@ if(typeof pConfig.ABE_WEBSITE !== 'undefined' && pConfig.ABE_WEBSITE !== null) {
 
 	Manager.instance.init()
 		.then(function () {
-			var shouldSaveXml = false;
 			var list = Manager.instance.getList();
-			var sitemapFileName = "sitemap.xml";
-			var xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-			xml += "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n";
+			var splitFolder = false;
 
-			var domain = ""
+			var domain = "";
+			var priorities = null;
+			var sitemapName = "sitemap.xml";
 			if(typeof abe.config.sitemap !== 'undefined' && abe.config.sitemap !== null) {
 				if(typeof abe.config.sitemap.domain !== 'undefined' && abe.config.sitemap.domain !== null) {
 					domain = abe.config.sitemap.domain.replace(/\/$/, '') + '/'
 				}
+				if(typeof abe.config.sitemap.priorities !== 'undefined' && abe.config.sitemap.priorities !== null) {
+					priorities = abe.config.sitemap.priorities
+				}
+				if(typeof abe.config.sitemap.sitemapName !== 'undefined' && abe.config.sitemap.sitemapName !== null) {
+					sitemapName = abe.config.sitemap.sitemapName
+				}
+				if(typeof abe.config.sitemap.folders !== 'undefined' && abe.config.sitemap.folders !== null) {
+					splitFolder = true
+				}
 			}
 
-			Array.prototype.forEach.call(list, (file) => {
-				if (file.publish) {
-					shouldSaveXml = true;
-					var d = new Date(file.publish.date);
-					var month = d.getUTCMonth() + 1; //months from 1-12
-					var day = d.getUTCDate();
-					var year = d.getUTCFullYear();
-
-					newdate = year + "-" + ((month < 10) ? "0" + month : month) + "-" + ((day < 10) ? "0" + day : day);
-
-					var priority = "0.5"
-					if(typeof abe.config.sitemap !== 'undefined' && abe.config.sitemap !== null
-						&& typeof abe.config.sitemap.prorities !== 'undefined' && abe.config.sitemap.prorities !== null
-						&& typeof abe.config.sitemap.prorities[file.abe_meta.template] !== 'undefined' && abe.config.sitemap.prorities[file.abe_meta.template] !== null) {
-						priority = abe.config.sitemap.prorities[file.abe_meta.template]
+			if (splitFolder) {
+				var folders = []
+				var regex = new RegExp(abe.config.sitemap.folders.regex)
+				Array.prototype.forEach.call(list, function(file) {
+					if (file.publish) {
+						var match = regex.exec(file.publish.html)
+						if(typeof match !== 'undefined' && match !== null
+							&& typeof match[1] !== 'undefined' && match[1] !== null) {
+							if(typeof folders[match[1]] === 'undefined' || folders[match[1]] === null) {
+								folders[match[1]] = []
+							}
+							folders[match[1]].push(file)
+						}
 					}
+				})
 
-				  xml += "  <url>\n";
-				  xml += "    <loc>" + domain + file.publish.html.replace(/^\//, '') + "</loc>\n";
-				  xml += "    <lastmod>" + newdate + "</lastmod>\n";
-				  xml += "    <priority>" + priority + "</priority>\n";
-				  xml += "  </url>\n";
+				var sitemapXml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+				sitemapXml += "<sitemapindex xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n";
+
+				Array.prototype.forEach.call(Object.keys(folders), (key) => {
+					var xml = createXml(folders[key], domain, priorities)
+					var name = key.replace(new RegExp('(' + key + ')'), abe.config.sitemap.folders.sitemapName)
+					if (xml !== false) {
+						var saveToPath = path.join(abe.config.root, abe.config.publish.url, name)
+						fs.writeFileSync(saveToPath, xml, {encoding: 'utf8'})
+					}
+					sitemapXml += "  <sitemap><loc>" + domain + name + "</loc></sitemap>\n";
+				})
+				sitemapXml += "</sitemapindex>";
+				saveToPath = path.join(abe.config.root, abe.config.publish.url, sitemapName)
+				fs.writeFileSync(saveToPath, sitemapXml, {encoding: 'utf8'})
+			}else {
+				var xml = createXml(list, domain, priorities)
+
+				if (xml !== false) {
+					var saveToPath = path.join(abe.config.root, abe.config.publish.url, sitemapName)
+					fs.writeFileSync(saveToPath, xml, {encoding: 'utf8'})
 				}
-			})
-			xml += "</urlset>";
-
-			if (shouldSaveXml) {
-				if(typeof abe.config.sitemap !== 'undefined' && abe.config.sitemap !== null
-							&& typeof abe.config.sitemap.sitemapFileName !== 'undefined' && abe.config.sitemap.sitemapFileName !== null) {
-					sitemapFileName = abe.config.sitemap.sitemapFileName
-				}
-				var saveToPath = path.join(abe.config.root, abe.config.publish.url, sitemapFileName)
-
-				fs.writeFileSync(saveToPath, xml, {encoding: 'utf8'})
 			}
 		})
 }
